3643c3f73dc9b0c7f8a6525185f28c6877abd636
James Gallicchio <jamesgallicchio@gmail.com>
2024-03-07 17:54:37 -0500

    systems code wahoo

---
+++ LeanSAT/Util/MkFIFO.lean
@@ -0,0 +1,30 @@
+/-
+Copyright (c) 2024 The LeanSAT Contributors.
+Released under the Apache License v2.0; see LICENSE for full text.
+
+Authors: James Gallicchio
+-/
+
+namespace LeanSAT.Util
+
+def mkFIFO (path : System.FilePath) : IO Unit := do
+  let child ← IO.Process.spawn {
+    cmd := "mkfifo"
+    args := #[path.toString]
+  }
+  if (← child.wait) > 0 then
+    throw (IO.userError "mkfifo failed")
+
+def getTempPath : IO System.FilePath := do
+  let name ← IO.Process.run {
+    cmd := "mktemp"
+    args := #["-u"]
+  }
+  return name
+
+def withTempFIFO (f : System.FilePath → IO α) : IO α := do
+  let name ← getTempPath
+  mkFIFO name
+  let res ← f name
+  IO.FS.removeFile name
+  return res
